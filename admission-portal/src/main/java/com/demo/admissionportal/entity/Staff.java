2183601429a349c857f8b5da2373c9e1f797a83d
DESKTOP-K0TQQAP\anh12 <anh123456bhh@gmail.com>
2024-06-16 22:42:32 +0700

    [Duy An] - [Add] : Add login staff

--- admission-portal/src/main/java/com/demo/admissionportal/entity/Staff.java
+++ admission-portal/src/main/java/com/demo/admissionportal/entity/Staff.java
@@ -1,15 +1,21 @@
 package com.demo.admissionportal.entity;
 
 import com.demo.admissionportal.constants.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.Nationalized;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 /**
  * The type Staff.
@@ -20,35 +26,36 @@ import org.hibernate.annotations.Nationalized;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "[staff]")
-public class Staff {
+public class Staff implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
+    @Column(name = "id")
     private Integer id;
 
     @NotNull
-    @Column(name = "username", nullable = false)
+    @Column(name = "username")
     private String username;
 
     @NotNull
     @Nationalized
-    @Column(name = "name", nullable = false)
+    @Column(name = "name")
     private String name;
 
     @NotNull
-    @Column(name = "email", nullable = false)
+    @Column(name = "email")
     private String email;
 
     @NotNull
-    @Column(name = "password", nullable = false)
+    @Column(name = "password")
     private String password;
 
     @NotNull
-    @Column(name = "avatar", nullable = false)
+    @Column(name = "avatar")
     private String avatar;
 
     @NotNull
-    @Column(name = "phone", nullable = false)
+    @Column(name = "phone"
+    )
     private String phone;
 
     @Enumerated(EnumType.STRING)
@@ -60,4 +67,33 @@ public class Staff {
     @ColumnDefault("'ACTIVE'")
     @Column(name = "status", nullable = false)
     private String status;
+
+    @OneToMany(mappedBy = "staff")
+    @JsonIgnore
+    private List<StaffToken> tokens;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
