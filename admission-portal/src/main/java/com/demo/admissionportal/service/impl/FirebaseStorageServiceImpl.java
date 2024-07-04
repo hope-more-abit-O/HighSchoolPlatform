2ee7d54bb70e58188f8c50d2fb1b04ee63a18aea
hopelessabit <ngocminh240402@gmail.com>
2024-07-04 15:11:08 +0700

    [NGOCMINH] [UPDATE]: firebase update

--- admission-portal/src/main/java/com/demo/admissionportal/service/impl/FirebaseStorageServiceImpl.java
+++ admission-portal/src/main/java/com/demo/admissionportal/service/impl/FirebaseStorageServiceImpl.java
@@ -22,7 +22,7 @@ public class FirebaseStorageServiceImpl implements FirebaseStorageService {
     /**
      * Uploads multiple files to Firebase Storage.
      *
-     * This method uploads each file in the provided `images` array to a separate location in the Firebase Storage bucket named "highschoolvn-28300.appspot.com". The filenames are generated using a random UUID and the original file extension. The method returns a comma-separated string containing all uploaded filenames.
+     * This method uploads each file in the provided `images` array to a separate location in the Firebase Storage bucket named "highschoolvn-dev.appspot.com". The filenames are generated using a random UUID and the original file extension. The method returns a comma-separated string containing all uploaded filenames.
      *
      * @param images An array of MultipartFile objects representing the files to upload.
      * @return A comma-separated string containing the filenames of the uploaded files.
@@ -31,7 +31,7 @@ public class FirebaseStorageServiceImpl implements FirebaseStorageService {
     public String uploadMultipleFiles(MultipartFile[] images) throws IOException {
         StringBuilder responseBuilder = new StringBuilder();
         List<String> response = new ArrayList<>(); // Build a response string
-        Bucket bucket = FirebaseUtil.getStorageClient().bucket("highschoolvn-28300.appspot.com");
+        Bucket bucket = FirebaseUtil.getStorageClient().bucket("highschoolvn-dev.appspot.com");
         for (MultipartFile imageFile : images) {
             String fileName = UUID.randomUUID().toString() + "." + getExtension(Objects.requireNonNull(imageFile.getOriginalFilename()));
             bucket.create(fileName, imageFile.getBytes(), imageFile.getContentType());
@@ -42,16 +42,16 @@ public class FirebaseStorageServiceImpl implements FirebaseStorageService {
     /**
      * Uploads a single file to Firebase Storage.
      *
-     * This method uploads the provided `file` to a location in the Firebase Storage bucket named "highschoolvn-28300.appspot.com". The filename is generated using a random UUID and the original file extension. The method returns the uploaded filename.
+     * This method uploads the provided `file` to a location in the Firebase Storage bucket named "highschoolvn-dev.appspot.com". The filename is generated using a random UUID and the original file extension. The method returns the uploaded filename.
      *
      * @param file A MultipartFile object representing the file to upload.
      * @return The filename of the uploaded file.
      * @throws IOException If an I/O error occurs during the upload process.
      */
     public String uploadFile(MultipartFile file) throws IOException {
-        Bucket bucket = FirebaseUtil.getStorageClient().bucket("highschoolvn-28300.appspot.com");
+        Bucket bucket = FirebaseUtil.getStorageClient().bucket("highschoolvn-dev.appspot.com");
         String fileName = UUID.randomUUID().toString() + "." + getExtension(Objects.requireNonNull(file.getOriginalFilename()));
-        FirebaseUtil.getStorageClient().bucket("highschoolvn-28300.appspot.com").create(fileName, file.getBytes(), file.getContentType());
+        FirebaseUtil.getStorageClient().bucket("highschoolvn-dev.appspot.com").create(fileName, file.getBytes(), file.getContentType());
         return fileName.toString();
     }
 
