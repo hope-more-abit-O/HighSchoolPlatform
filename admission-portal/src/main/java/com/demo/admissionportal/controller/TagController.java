90f28955d77ad571ad2224ab23bd595f941b211e
DESKTOP-K0TQQAP\anh12 <anh123456bhh@gmail.com>
2024-07-09 08:33:58 +0700

    [Duy An] - [Rename] : Rename controller

--- admission-portal/src/main/java/com/demo/admissionportal/controller/TagController.java
+++ admission-portal/src/main/java/com/demo/admissionportal/controller/TagController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/post/tag")
 @RequiredArgsConstructor
-public class PostController {
+public class TagController {
     private final TagService tagService;
 
     @PostMapping()
