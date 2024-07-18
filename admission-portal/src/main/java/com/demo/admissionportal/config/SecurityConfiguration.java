1d988c7b581ebba31113fc32992a321c5c1924e9
DESKTOP-K0TQQAP\anh12 <anh123456bhh@gmail.com>
2024-07-18 11:56:39 +0700

    [Duy An] - [Fix] : Fix CORS

--- admission-portal/src/main/java/com/demo/admissionportal/config/SecurityConfiguration.java
+++ admission-portal/src/main/java/com/demo/admissionportal/config/SecurityConfiguration.java
@@ -93,7 +93,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
