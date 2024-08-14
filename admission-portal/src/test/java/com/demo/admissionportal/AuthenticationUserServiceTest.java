10a97de2f473956ddf56fb6b045bdbf51276b86d
dyhieu <buihieuduy@gmail.com>
2024-08-14 13:13:29 +0700

    page size & handle loop

--- admission-portal/src/test/java/com/demo/admissionportal/AuthenticationUserServiceTest.java
+++ admission-portal/src/test/java/com/demo/admissionportal/AuthenticationUserServiceTest.java
@@ -36,7 +36,6 @@ import org.mockito.*;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.io.IOException;
