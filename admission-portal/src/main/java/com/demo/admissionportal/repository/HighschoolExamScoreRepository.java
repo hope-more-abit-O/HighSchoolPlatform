fca268241e741a3eb8bd099c47a8e1d0542ae252
hopelessabit <ngocminh240402@gmail.com>
2024-09-19 00:19:37 +0700

    Revert "Merge branch 'dev' of https://github.com/hope-more-abit-O/HighSchoolPlatform into dev"
    This reverts commit 411ae797e0818c3f5e9e325039302002fdc6c8ab, reversing
    changes made to 46e10da37aaf783b1c842994a9f8b8a7393b2b46.

--- admission-portal/src/main/java/com/demo/admissionportal/repository/HighschoolExamScoreRepository.java
+++ admission-portal/src/main/java/com/demo/admissionportal/repository/HighschoolExamScoreRepository.java
@@ -106,13 +106,6 @@ List<HighschoolExamScore> findAllByYearAndLocalAndStatus(@Param("year") Integer 
             "WHERE ey.year = :year")
     Page<HighschoolExamScore> findByExamYear(@Param("year") Integer year, Pageable pageable);
 
-    @Query("""
-             SELECT h
-             FROM HighschoolExamScore h
-             WHERE h.identificationNumber = :identificationNumber
-             AND h.score IS NOT NULL
-""")
-    List<HighschoolExamScore> findByIdentificationNumberAndScoreIsNotNull(String identificationNumber);
 }
 
 
