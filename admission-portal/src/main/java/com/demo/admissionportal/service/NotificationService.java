a1029740a07b9a507c3f95cfb073e2d3e0e9f161
DESKTOP-K0TQQAP\anh12 <anh123456bhh@gmail.com>
2024-09-25 13:43:31 +0700

    [Duy An] - [Add] : Add API Push notification

---
+++ admission-portal/src/main/java/com/demo/admissionportal/service/NotificationService.java
@@ -0,0 +1,13 @@
+package com.demo.admissionportal.service;
+
+/**
+ * The interface Notification service.
+ */
+public interface NotificationService {
+    /**
+     * Gets access token.
+     *
+     * @return the access token
+     */
+    public String getAccessToken();
+}
