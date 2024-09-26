e3140b968516a768a171d77d1b3dfb9eb8fc51d4
Duy Hieu <buihieuduy@gmail.com>
2024-09-26 16:56:05 +0700

    modified create exam scores

--- admission-portal/src/main/java/com/demo/admissionportal/repository/ExamLocalRepository.java
+++ admission-portal/src/main/java/com/demo/admissionportal/repository/ExamLocalRepository.java
@@ -3,12 +3,17 @@ package com.demo.admissionportal.repository;
 
 import com.demo.admissionportal.entity.ExamLocal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 public interface ExamLocalRepository extends JpaRepository<ExamLocal, Integer> {
     ExamLocal findByName(String local);
 
     List<ExamLocal> findByIdIn(Set<Integer> examLocalIds);
+
+    @Query("SELECT e.name FROM ExamLocal e WHERE e.id = :localId")
+    Optional<String> findLocalNameById(Integer localId);
 }
